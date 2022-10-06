74a9b21677bf24532f19adfbf3193303a45e9dc9
Babul Akter <babulakterfsd@gmail.com>
2022-10-06 20:16:19 +0600

    product data updated

--- src/redux/products/productReducer.js
+++ src/redux/products/productReducer.js
@@ -1,22 +1,25 @@
 const initialState = {
     products: [
         {
-            id: '1',
+            id: 1,
             title: 'Asus Vivobook x515MA',
             price: 35500,
             quantity: 10,
+            stock: 10,
         },
         {
-            id: '2',
+            id: 2,
             title: 'DEll Inspiron 15 3000',
             price: 93000,
             quantity: 7,
+            stock: 7,
         },
         {
-            id: '3',
+            id: 3,
             title: 'Canon EOS 1500D',
             price: 65000,
             quantity: 12,
+            stock: 12,
         },
     ],
 };
